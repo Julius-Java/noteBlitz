9b5ba37688cff6685162c0eaba9e2f702187e614
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2023-08-27 01:03:22 +0100

    Refactor Task List component test

--- components/__test__/TaskList.test.jsx
+++ components/__test__/TaskList.test.jsx
@@ -1,11 +1,23 @@
 import { render, screen } from "@testing-library/react"
 import TaskList from "../TaskList"
+import { useTaskContext } from '../TaskContext';
+jest.mock('../TaskContext'); // Mock the TaskContext module
 
 
 describe("Task List Render", () => {
-    it("Renders", () => {
-        render(<TaskList />)
-    })
+    it("Renders without crashing", () => {
+        // Mock the context values you want to use for testing
+        const mockState = {
+            tasks: [
+                { id: 1, item: "Task 1", completed: true },
+                { id: 2, item: "Task 2", completed: false },
+                // ... more tasks
+            ]
+        };
+        const mockDispatch = jest.fn(); // Mock the dispatch function
+        useTaskContext.mockReturnValue({ state: mockState, dispatch: mockDispatch });
+        render(<TaskList />); // Render the component
+    });
 
     it("Renders a List component", () => {
         render(<TaskList />)
